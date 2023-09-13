0cfdbe1da40d6f552e759efe802ada93e9f9ea37
LAPTOP-N0B1SC8S\Lenovo <pospark@gemplatforms.com>
2023-09-13 16:14:24 +0900

    commit

--- src/views/GridListPage.vue
+++ src/views/GridListPage.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <v-container>
-      Hello
+      <v-row>
+        <v-col
+        v-for="count in 12"
+        :key="count"
+        cols="3"
+        sm>
+          <v-card class="pa-3" outlined>
+            col
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
